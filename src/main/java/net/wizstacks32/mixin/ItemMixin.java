3b547bde02102ff16310fb14f56d6bd0e6886531
jeans130 <jensmonne@gmail.com>
2024-02-07 08:55:39 +0100

    init1

--- src/main/java/net/wizstacks32/mixin/ItemMixin.java
+++ src/main/java/net/wizstacks32/mixin/ItemMixin.java
@@ -1,6 +1,8 @@
 package net.wizstacks32.mixin;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MusicDiscItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
@@ -9,5 +11,12 @@ public class ItemMixin {
 	@Overwrite
 	public final int getMaxCount() {
 		return 32;
+	}
+	public int getMaxCount(ItemStack stack) {
+        if (stack.getItem() instanceof MusicDiscItem) {
+            return 1;
+        } else {
+            return stack.getItem().getMaxCount();
+        }
 	}
 }
